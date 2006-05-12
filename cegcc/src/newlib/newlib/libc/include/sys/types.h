deea73f33130f13720ddd2773da0d35fc8f6691f
pedroalves <pedroalves@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-05-12 17:49:36 +0000

    always hide fd_set and friends for wince.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@30 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/src/newlib/newlib/libc/include/sys/types.h
+++ cegcc/src/newlib/newlib/libc/include/sys/types.h
@@ -209,7 +209,7 @@ typedef unsigned short nlink_t;
    includes the W32api winsock[2].h header must know what it is doing;
    it must not call the cygwin32 select function.
 */
-# if !(defined (_POSIX_SOURCE) || defined (_WINSOCK_H) || defined (__USE_W32_SOCKETS)) 
+# if !(defined (_POSIX_SOURCE) || defined (_WINSOCK_H) || defined (__USE_W32_SOCKETS) || defined (UNDER_CE)) 
 #  define _SYS_TYPES_FD_SET
 #  define	NBBY	8		/* number of bits in a byte */
 /*
