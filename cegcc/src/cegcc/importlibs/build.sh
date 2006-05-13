24de5c579511f5b08be8d047f51cb3475da55793
pedroalves <pedroalves@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-05-13 17:47:00 +0000

    generate some more libs
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@49 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/src/cegcc/importlibs/build.sh
+++ cegcc/src/cegcc/importlibs/build.sh
@@ -7,5 +7,10 @@ outputdir=$2
 ./mkimport.sh $defdir/icmplib.def $outputdir && \
 ./mkimport.sh $defdir/note_prj.def $outputdir && \
 ./mkimport.sh $defdir/iphlpapi.def $outputdir && \
+./mkimport.sh $defdir/commctrl.def $outputdir && \
+./mkimport.sh $defdir/ole32.def $outputdir && \
+./mkimport.sh $defdir/oleaut32.def $outputdir && \
+./mkimport.sh $defdir/aygshell.def $outputdir && \
+./mkimport.sh $defdir/ceshell.def $outputdir && \
 ./winsock.sh $defdir $outputdir && \
 ./ws2.sh $defdir $outputdir 
