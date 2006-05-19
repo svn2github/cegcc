2b59a24ab030849d44bafcecd110a56cdb25bcf4
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-05-19 20:24:24 +0000

    I set the property wrong the other time.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@77 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/scripts/update_news.sh
+++ cegcc/scripts/update_news.sh
@@ -4,6 +4,6 @@ ATOM=atom.xml
 HTML=news_table.html
 
 mv -f ${ATOM} ${ATOM}.prev
-mv -f ${HTML} ${HTML}.prev
+# mv -f ${HTML} ${HTML}.prev
 wget http://cegcc.blogspot.com/atom.xml -O ${ATOM}
 php -f $NIGHTLY_DIR/scripts/blogger.php > $WEB_DIR/${HTML}
