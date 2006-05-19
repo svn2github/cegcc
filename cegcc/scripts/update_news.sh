6493321bb25563f7c790717ae0b00c917ef56d70
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-05-19 17:46:58 +0000

    Set update_news.sh properties so it'll use normal end-of-line on linux. Pass variables, adapt some paths.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@76 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/scripts/update_news.sh
+++ cegcc/scripts/update_news.sh
@@ -6,4 +6,4 @@ HTML=news_table.html
 mv -f ${ATOM} ${ATOM}.prev
 mv -f ${HTML} ${HTML}.prev
 wget http://cegcc.blogspot.com/atom.xml -O ${ATOM}
-php -f blogger.php > ${HTML}
+php -f $NIGHTLY_DIR/scripts/blogger.php > $WEB_DIR/${HTML}
