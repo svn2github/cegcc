248ebc02d11d5f05b78460dc02d26e969a2aab95
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-05-20 05:32:17 +0000

    Maybe this'll do the trick to integrate news.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@82 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/scripts/update_news.sh
+++ cegcc/scripts/update_news.sh
@@ -6,4 +6,12 @@ HTML=news_table.html
 mv -f ${ATOM} ${ATOM}.prev
 # mv -f ${HTML} ${HTML}.prev
 wget http://cegcc.blogspot.com/atom.xml -O ${ATOM}
-php -f $NIGHTLY_DIR/scripts/blogger.php > $WEB_DIR/${HTML}
+php -f $NIGHTLY_DIR/scripts/blogger.php > ${HTML}
+
+#
+# CUT the index file in two pieces, put the new stuff in between.
+#
+LN=`grep -n "CUT HERE" $WEB_DIR/index.html | awk -F: '{print $1;}'`
+head -$LN $WEB_DIR/index.html >ix1
+tail +$LN $WEB_DIR/index.html >ix2
+cat ix1 ${HTML} ix2 >$WEB_DIR/index.html
