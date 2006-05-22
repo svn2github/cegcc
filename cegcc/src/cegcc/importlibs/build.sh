841d927cbc0948a6dbfb53f00033d30a9ece64a7
pedroalves <pedroalves@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-05-22 13:20:02 +0000

    move && logic to (|| exit) logic, better than having a command that spawns multiple lines.
    don't rename symbols in winsock and ws2 anymore.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@108 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/src/cegcc/importlibs/build.sh
+++ cegcc/src/cegcc/importlibs/build.sh
@@ -3,14 +3,16 @@
 defdir=$1
 outputdir=$2
 
-./mkimport.sh $defdir/coredll.def $outputdir && \
-./mkimport.sh $defdir/icmplib.def $outputdir && \
-./mkimport.sh $defdir/note_prj.def $outputdir && \
-./mkimport.sh $defdir/iphlpapi.def $outputdir && \
-./mkimport.sh $defdir/commctrl.def $outputdir && \
-./mkimport.sh $defdir/ole32.def $outputdir && \
-./mkimport.sh $defdir/oleaut32.def $outputdir && \
-./mkimport.sh $defdir/aygshell.def $outputdir && \
-./mkimport.sh $defdir/ceshell.def $outputdir && \
-./winsock.sh $defdir $outputdir && \
-./ws2.sh $defdir $outputdir 
+./mkimport.sh $defdir/coredll.def $outputdir || exit
+./mkimport.sh $defdir/icmplib.def $outputdir || exit
+./mkimport.sh $defdir/note_prj.def $outputdir || exit
+./mkimport.sh $defdir/iphlpapi.def $outputdir || exit
+./mkimport.sh $defdir/commctrl.def $outputdir || exit
+./mkimport.sh $defdir/ole32.def $outputdir || exit
+./mkimport.sh $defdir/oleaut32.def $outputdir || exit
+./mkimport.sh $defdir/aygshell.def $outputdir || exit
+./mkimport.sh $defdir/ceshell.def $outputdir || exit
+./mkimport.sh $defdir/winsock.def $outputdir || exit
+./mkimport.sh $defdir/ws2.def $outputdir || exit
+#./winsock.sh $defdir $outputdir || exit
+#./ws2.sh $defdir $outputdir 
