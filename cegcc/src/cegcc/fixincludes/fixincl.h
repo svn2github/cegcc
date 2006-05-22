f0a9e9f2fc616584de71c2d76c017b720c82b055
pedroalves <pedroalves@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-05-22 23:56:38 +0000

    * fixincl.h (defines) : Don't define WINCEMACRO, that should not be defined by default, in standard code.                         WIN32 and _WIN32 are defined by the specs when -mwin32 is passed, remove.                         UNICODE, _UNICODE and _MT are always defined, remove.                         No need to define size_t.                         Don't include <wchar.h> just for wint_t, typedef it here.                         Remove WCHAR, HANDLE, VOID, CHAR, SHORT, LONG typedefs.                         Remove unneeded header inclusion.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@130 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/src/cegcc/fixincludes/fixincl.h
+++ cegcc/src/cegcc/fixincludes/fixincl.h
@@ -3,14 +3,10 @@
 
 //for compatibility with MS headers
 
-#define WINCEMACRO
 #define ARM 1
 #define _ARM_ 1
 #define ARMV4 1
 #define ARM_WINCE 1
-// in some cases #defining _WIN32 may help, but when compiling GCC it causes problems
-//#define _WIN32
-//#define WIN32
 
 #ifndef UNDER_CE
 #define UNDER_CE 420
@@ -29,16 +25,6 @@
 #define __inline static inline
 #define _inline __inline
 
-#ifndef UNICODE
-#define UNICODE
-#endif
-
-#ifndef _UNICODE
-#define _UNICODE
-#endif
-
-#define _MT
-
 #ifndef __STDC_HOSTED__
 #define __STDC_HOSTED__ 1
 #define __ARM_ARCH_4__
@@ -56,58 +42,11 @@
 
 #define _FORCENAMELESSUNION
 
-//size_t and wchar_t are defined in many different places in all SDKs.
-//let's put them here (just to be sure)
-
-#ifndef _SIZE_T_DEFINED
-typedef unsigned int size_t;
-//typedef long unsigned int size_t;
-#define _SIZE_T_DEFINED
+#ifndef __cplusplus
+typedef short unsigned int wchar_t;
 #endif
 
-#include <wchar.h>
-
-#if 0
-#include <wchar.h>
-#ifndef _WCHAR_T_DEFINED
-# define _WCHAR_T_DEFINED
-#endif
-
-typedef wchar_t WCHAR;
-typedef WCHAR TCHAR;
-
-typedef void* HANDLE;
-#define DECLARE_HANDLE(name) typedef HANDLE name
-
-#ifndef VOID
-#define VOID void
-typedef char CHAR;
-typedef short SHORT;
-typedef long LONG;
-#endif
-
-#include <stddef.h>
-#include <basetsd.h>
-#include <winnt.h>
-#include <windef.h>
-#include <basetsd.h>
-#include <winbase.h>
-#include <wtypes.h>
-
-#if 0
-//ptrdiff_t is defined in stddef.h from the sdk
-#ifndef _PTRDIFF_T_DEFINED
-typedef int ptrdiff_t;
-#define _PTRDIFF_T_DEFINED
-#endif
-
-#ifndef _WCTYPE_T_DEFINED
-//typedef wchar_t wint_t;
-//typedef wchar_t wctype_t;
-#define _WCTYPE_T_DEFINED
-#endif
-#endif
-
-#endif
+typedef unsigned short wint_t;
+//#undef ULONG
 
 #endif
