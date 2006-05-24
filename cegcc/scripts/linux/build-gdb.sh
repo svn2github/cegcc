9167f925e3b1f09d35152cd14b8bfee10929c3b5
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-05-24 21:06:04 +0000

    Add a script to build GDB.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@139 d7810a3d-100a-0410-8641-c3624a9c11f1

---
+++ cegcc/scripts/linux/build-gdb.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -x
+#
+# Read the settings
+#
+if [ -r settings.sh ]; then
+	. settings.sh
+else
+	. scripts/linux/settings.sh
+fi
+#
+# Put the cleanup here instead of in the calling script
+#
+if [ -d $BUILD_DIR/gdb ]; then
+	rm -rf $BUILD_DIR/gdb
+fi
+#
+mkdir -p $BUILD_DIR/gdb
+cd $BUILD_DIR/gdb
+#
+$TOP_SRCDIR/src/gdb/configure --prefix=$PREFIX || exit 1
+make || exit 1
+exit 0
