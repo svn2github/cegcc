0c7fee8d3d78e931edb8e65beef74a54ad4e9918
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-05-31 21:08:47 +0000

    Add a script to install the include files from src/w32api.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@154 d7810a3d-100a-0410-8641-c3624a9c11f1

---
+++ cegcc/scripts/linux/install-includes.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# This won't work, the MinGW build system isn't configured for this.
+#
+# $TOP_SRCDIR/src/w32api/configure \
+#	 --prefix=$PREFIX \
+#	--target=$TARGET_ARCH
+# make
+#
+# Instead we're relying on the fact that the w32api/include contains
+# a directory tree that we should copy as is.
+#
+if [ -r settings.sh ]; then
+	. settings.sh
+else
+	. scripts/linux/settings.sh
+fi
+#
+cd $TOP_SRCDIR/src/w32api/include
+DIRS=`find . -type d -name .svn -prune -print`
+echo "Include file directories : [$DIRS]"
+for d in $DIRS
+do
+	mkdir -p $PREFIX/$TARGET_ARCH/include/w32api/$d
+done
+FILES=`find . -type f -print | grep -v /.svn/ `
+echo "Files : [" $FILES "]"
+for i in $FILES
+do
+	install -m 0644 $i $PREFIX/$TARGET_ARCH/include/w32api/$i
+done
+exit 0
