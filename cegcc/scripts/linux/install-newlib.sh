ac5ed85af47173f661552c9efaf236ebf32d1415
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-06-08 19:40:01 +0000

    Add error handing and proper init.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@170 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/scripts/linux/install-newlib.sh
+++ cegcc/scripts/linux/install-newlib.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+#
+# Read the settings
+#
+if [ -r settings.sh ]; then
+	. settings.sh
+else
+	. scripts/linux/settings.sh
+fi
+#
 cd $BUILD_DIR/newlib
-make install
+#
+make install || exit 1
 exit 0
