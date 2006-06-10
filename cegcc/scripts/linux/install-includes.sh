5b97e85ed8479b074a263a836a8d605839164e6a
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-06-10 11:45:19 +0000

    Clean this up a bit.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@177 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/scripts/linux/install-includes.sh
+++ cegcc/scripts/linux/install-includes.sh
@@ -17,14 +17,14 @@ else
 fi
 #
 cd $TOP_SRCDIR/src/w32api/include
-DIRS=`find . -type d -name .svn -prune -print`
-echo "Include file directories : [$DIRS]"
+DIRS=`find . -type d -print | grep -v /.svn`
+# echo "Include file directories : [$DIRS]"
 for d in $DIRS
 do
 	mkdir -p $PREFIX/$TARGET_ARCH/include/w32api/$d
 done
-FILES=`find . -type f -print | grep -v /.svn/ `
-echo "Files : [" $FILES "]"
+FILES=`find . -type f -name \*.h -print | grep -v /.svn/ `
+# echo "Files : [" $FILES "]"
 for i in $FILES
 do
 	install -m 0644 $i $PREFIX/$TARGET_ARCH/include/w32api/$i
