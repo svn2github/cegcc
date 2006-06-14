e82934b99009b2bf7d825f9f2cc3bc4475cd0ba0
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-06-14 21:03:18 +0000

    Error handling and a typo.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@190 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/scripts/linux/install-gcc.sh
+++ cegcc/scripts/linux/install-gcc.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
+#
+# Read the settings
+#
+if [ -r settings.sh ]; then
+	. settings.sh
+else
+	. scripts/linux/settings.sh
+fi
 cd $BUILD_DIR/gcc
 #
 # It is very important to specify the right target here.
 #
-make install-gcc
+make install-gcc || exit 1
 #
 # An apparent problem with the GCC installation
 #
-GCC_LIBS_LIST="c a m"
+GCC_LIBS_LIST="c g m"
 for i in $GCC_LIBS_LIST
 do
-	$TARGET_ARCH-ranlib $PREFIX/$TARGET_ARCH/lib/lib$i.a
+	$TARGET_ARCH-ranlib $PREFIX/$TARGET_ARCH/lib/lib$i.a || exit 1
 done
 #
 exit 0
