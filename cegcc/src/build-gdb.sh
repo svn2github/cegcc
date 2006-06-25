dd92b080e9733058a8eeb7134d145548074fbb31
pedroalves <pedroalves@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-06-25 17:28:19 +0000

    Commit what I have been using for a long time. The stub is not built by default now. use build-stub.sh for that.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@211 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/src/build-gdb.sh
+++ cegcc/src/build-gdb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BUILD_DIR="build-gdb"
-GDB_VERSION=6.3-local
+GDB_VERSION=
 BASE_DIRECTORY=${PWD}
 
 mkdir -p ${BUILD_DIR} || exit
@@ -9,11 +9,6 @@ mkdir -p ${BUILD_DIR} || exit
 export TARGET="arm-wince-pe"
 export PREFIX="/usr/local"
 export PATH=${PATH}:${PREFIX}/bin
-
-#stuff for stub
-SDKDIR="/cygdrive/d/gcc/wince/sdk"
-INCLUDES="-idirafter ${SDKDIR}/Include/Armv4"
-LIBS="-L ${SDKDIR}/Lib/Armv4 -nodefaultlibs -nostdlib -lcorelibc -lwinsock -lcoredll"
 
 ########################################
 # BUILD GDB
@@ -23,13 +18,12 @@ echo ""
 echo ""
 
 cd ${BUILD_DIR} || exit
-${BASE_DIRECTORY}/gdb-${GDB_VERSION}/configure \
+${BASE_DIRECTORY}/gdb${GDB_VERSION}/configure \
   --with-gcc                     \
   --with-gnu-ld                  \
   --with-gnu-as                  \
   --target=${TARGET}             \
   --prefix=${PREFIX}             \
-  --disable-threads              \
   --disable-nls                  \
   --disable-win32-registry       \
   --disable-multilib             \
@@ -40,16 +34,6 @@ ${BASE_DIRECTORY}/gdb-${GDB_VERSION}/configure \
 
 make         || exit
 make install || exit
-
-echo ""
-echo "BUILDING STUB --------------------"
-echo ""
-
-EXE="${PREFIX}/bin/${TARGET}-stub.exe"
-SOURCES="${BASE_DIRECTORY}/gdb-${GDB_VERSION}/gdb/wince-stub.c"
-
-${TARGET}-gcc ${SOURCES} -o ${EXE} -e WinMainCRTStartup ${LIBS} ${INCLUDES} || exit
-${TARGET}-strip ${EXE} || exit
 
 cd ${BASE_DIRECTORY} || exit
 
