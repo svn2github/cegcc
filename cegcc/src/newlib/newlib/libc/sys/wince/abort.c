7212148d120dda94be98af32536a1c3e7f84cbdd
pedroalves <pedroalves@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-08-20 21:12:57 +0000

    Remove MessageBox and loop. Simply _exit with error. Needed for running gdb testsuite.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@317 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/src/newlib/newlib/libc/sys/wince/abort.c
+++ cegcc/src/newlib/newlib/libc/sys/wince/abort.c
@@ -11,18 +11,5 @@ _DEFUN_VOID (abort)
 #ifdef ABORT_MESSAGE
   write (2, "Abort called\n", sizeof ("Abort called\n")-1);
 #endif
-
-  wchar_t buf[256];
-  GetModuleFileNameW(NULL, buf, sizeof(buf));
-  MessageBoxW(0, buf, L"Abort!", 0);
-  DebugBreak();
-  TerminateProcess(0,0);
   _exit (1);
-
-  while (1)
-    {
-      raise (SIGABRT);
-	  DebugBreak();
-      _exit (1);
-    }
 }
