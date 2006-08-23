525a5722cc553956975c1e747d4d930f039e0a27
pedroalves <pedroalves@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-08-23 17:58:28 +0000

    Add IRDA_DEVICE_INFO and DEVICELIST, found at http://msdn.microsoft.com/library/default.asp?url=/library/en-us/wcecomm5/html/wce51lrfdevicelist.asp
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@338 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/src/w32api/include/af_irda.h
+++ cegcc/src/w32api/include/af_irda.h
@@ -7,8 +7,20 @@ typedef struct _SOCKADDR_IRDA {
   u_short irdaAddressFamily;
   u_char irdaDeviceID[4];
   char irdaServiceName[25];
-} SOCKADDR_IRDA, 
- *PSOCKADDR_IRDA, 
- *LPSOCKADDR_IRDA;
+} SOCKADDR_IRDA, *PSOCKADDR_IRDA, *LPSOCKADDR_IRDA;
+
+typedef struct IRDA_DEVICE_INFO {
+  u_char irdaDeviceID[4];
+  char irdaDeviceName[22];
+  u_char Reserved[2];
+} _IRDA_DEVICE_INFO;
+typedef _IRDA_DEVICE_INFO IRDA_DEVICE_INFO;
+
+typedef struct DEVICELIST {
+  ULONG numDevice;
+  IRDA_DEVICE_INFO Device [1];
+} _DEVICELIST, *PWCE_DEVICELIST;
+
+typedef _DEVICELIST DEVICELIST;
 
 #endif
