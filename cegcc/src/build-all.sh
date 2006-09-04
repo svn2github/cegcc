4fdb987ea1cd76543ffbe6fd6a28e1361b4463af
pedroalves <pedroalves@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-09-04 18:48:30 +0000

    Might as well be consistent...
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@538 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/src/build-all.sh
+++ cegcc/src/build-all.sh
@@ -18,7 +18,7 @@ echo "source: ${BASE_DIRECTORY}"
 echo "build: ${BUILD_DIR}"
 echo "prefix: ${PREFIX}"
 
-export PATH=${PREFIX}/bin:$PATH
+export PATH=${PREFIX}/bin:${PATH}
 
 mkdir -p ${BUILD_DIR} || exit 1
 mkdir -p ${PREFIX} || exit 1
