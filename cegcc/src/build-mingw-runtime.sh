fd9d7e50adc77727a5c3bf5b526b0c9c426e011d
pedroalves <pedroalves@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-09-06 11:57:49 +0000

    Make it build on linux too.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@561 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/src/build-mingw-runtime.sh
+++ cegcc/src/build-mingw-runtime.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-BUILD_DIR="build-mingw-runtime"
-BASE_DIRECTORY=${PWD}
-SOURCE_DIR=${BASE_DIRECTORY}/mingw
+########################################
+# BUILD MinGW runtime
 
+BUILD_DIR="build-mingw32ce"
+MINGW_VERSION=
+SOURCE_DIR=${BASE_DIRECTORY}/mingw${GCC_VERSION}
 
-mkdir -p ${BUILD_DIR} || exit
+mkdir -p ${BUILD_DIR} || exit 1
 
-export TARGET="arm-wince-mingw32"
-export PREFIX="/usr/local/mingw32ce"
+export BASE_DIRECTORY=${PWD}
+export BUILD=`sh ${SOURCE_DIR}/config.guess`
+export HOST="arm-wince-mingw32"
+export TARGET=${HOST}
+export PREFIX="/opt/mingw32ce"
 export PATH=${PATH}:${PREFIX}/bin
 
 #I have this normally set by ccache.
@@ -16,28 +21,33 @@ export PATH=${PATH}:${PREFIX}/bin
 #uses $host==$target, and CC instead of CC_FOR_TARGET.
 unset CC
 unset CXX
+
 #export CFLAGS="-g3 -O0"
 
 echo ""
-echo "BUILDING MinGW runtime (WinCE)-----------------------"
+echo "BUILDING MinGW(CE) runtime for: ${TARGET} -----------"
 echo ""
 echo ""
 
+echo BUILD=${BUILD}
+echo HOST=${HOST}
+echo TARGET=${TARGET}
 echo SOURCE_DIR=${SOURCE_DIR}
 echo BUILD_DIR=${BUILD_DIR}
 echo PREFIX=${PREFIX}
 
-cd ${BUILD_DIR} || exit
-${SOURCE_DIR}/configure \
-  --build=i686-pc-cygwin \
-  --host=${TARGET} \
-  --target=${TARGET} \
-  --prefix=${PREFIX} \
-  || exit
+cd ${BUILD_DIR} || exit 1
 
-make || exit
-make install || exit
-cd ${BASE_DIRECTORY} || exit
+${SOURCE_DIR}/configure          \
+  --build=${BUILD}               \
+  --host=${HOST}                 \
+  --target=${TARGET}             \
+  --prefix=${PREFIX}             \
+  || exit 1
+
+make || exit 1
+make install || exit 1
+cd ${BASE_DIRECTORY} || exit 1
 
 echo ""
 echo "DONE --------------------------"
