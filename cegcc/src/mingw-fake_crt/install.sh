b34a7d495e736cf592ad004fa6f3d0b1c2c6a37e
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-09-07 19:00:47 +0000

    Make this script more versatile.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@575 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/src/mingw-fake_crt/install.sh
+++ cegcc/src/mingw-fake_crt/install.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
-
-if [ $# -lt 1 ] ; then
-        echo "usage:"
-        echo "$0 [prefix dir]"
-        exit 1
+#
+# This script can now run in its source directory or from the
+# scripts/linux scripts, and either pick up the PREFIX from the
+# environment or from the command line.
+#
+if [ "x$PREFIX" = "x" ]; then
+	if [ $# -lt 1 ] ; then
+		echo "usage:"
+		echo "$0 [prefix dir]"
+		exit 1
+	fi
+	PREFIX=$1
 fi
 
-
-PREFIX=$1
+if [ "x$BUILD_DIR" != "x" ]; then
+	MY_DIR=$BUILD_DIR/mingw-libs
+	mkdir -p ${MY_DIR}
+	cd $TOP_SRCDIR/src/mingw-fake_crt
+else
+	MY_DIR=.
+fi
 TARGET=arm-wince-mingw32
 LIBDIR=${PREFIX}/${TARGET}/lib
 
@@ -16,8 +28,8 @@ AR=${TARGET}-ar
 
 FAKE_LIBS="libcoldname.a libmingwex.a libmoldname.a libmingw32.a libmingwthrd.a libmoldnamed.a"
 
-${AS} crt2.s -o crt2.o
-cp -fv crt2.o ${LIBDIR}/crt2.o
+${AS} crt2.s -o ${MY_DIR}/crt2.o
+cp -fv ${MY_DIR}/crt2.o ${LIBDIR}/crt2.o
 
 for lib in $FAKE_LIBS; do
    rm -f ${LIBDIR}/$lib
