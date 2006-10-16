f1440ae889099d309ab14ee75d6984a3e6b62f6c
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-10-16 19:07:07 +0000

    Add profile directory to show what you can do. This currently works for arm-wince-mingw32ce, not for arm-wince-cegcc.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@727 d7810a3d-100a-0410-8641-c3624a9c11f1

---
+++ cegcc/test/profile/m2.c
@@ -0,0 +1,22 @@
+#include <windows.h>
+
+
+/*
+ * Profiling test
+ */
+extern void func_a(int i);
+extern int fibo(int x);
+extern int func_b(int i);
+
+main(int argc, char *argv[])
+{
+	int	i;
+
+	for (i=1; i<10; i++) {
+		func_a(i);
+	}
+
+	for (i=1; i<30; i++) {
+		(void) func_b(i);
+	}
+}
