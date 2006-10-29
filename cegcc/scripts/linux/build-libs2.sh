57595e35cca1e55652f98944ff3644898d62c0d5
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-10-29 18:13:51 +0000

    Add a parameter so we can use the Makefile for both cegcc and mingw32ce.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@753 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/scripts/linux/build-libs2.sh
+++ cegcc/scripts/linux/build-libs2.sh
@@ -8,25 +8,22 @@ else
 	. scripts/linux/settings.sh
 fi
 #
-#if [ -d $BUILD_DIR/libs ]; then
-#	rm -rf $BUILD_DIR/libs
-#fi
+# Non-standard build, for both libs.
 #
-# Not in the MingW target
-#
-if [ $TGT_ARCH = arm-wince-mingw32ce ]; then
-	exit 0
+if [ $TGT_ARCH = arm-wince-cegcc ]; then
+	cd $TOP_SRCDIR/src/cegcc/cegccthrd || exit 1
+	make clean TARGET=$TGT_ARCH || exit 1
+	make all PREFIX=$PREFIX TARGET=$TGT_ARCH || exit 1
 fi
-#
-# Non-standard build
-#
-cd $TOP_SRCDIR/src/cegcc/cegccthrd || exit 1
-make clean TARGET=$TGT_ARCH || exit 1
-make all PREFIX=$PREFIX TARGET=$TGT_ARCH || exit 1
 #
 cd $TOP_SRCDIR/src/cegcc/libstdc++ || exit 1
 make clean TARGET=$TGT_ARCH || exit 1
-make all PREFIX=$PREFIX TARGET=$TGT_ARCH || exit 1
+if [ $TGT_ARCH = arm-wince-mingw32ce ]; then
+	TSL="-lcoredll"
+else
+	TSL="-lcegcc -lcoredll"
+fi
+make all PREFIX=$PREFIX TARGET=$TGT_ARCH THE_SYSTEM_LIBS=${TSL} || exit 1
 #
 # End
 #
