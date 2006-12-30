a8719157a850cb18b4421a98d5b54b9a9d3eb362
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2006-12-30 15:37:30 +0000

    Removing old files.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@858 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/scripts/linux/cegcc.spec
+++ cegcc/scripts/linux/cegcc.spec
@@ -5,9 +5,9 @@ Release:	1
 License:	open
 Packager:	Danny Backx <dannybackx@users.sourceforge.net>
 Group:		Development/Tools
-Prefixes:	/usr/ppc
+Prefixes:	/opt
 # Source:		http://sourceforge.net/project/showfiles.php?group_id=173455
-Source:		/tmp/cegcc-src-0.11.tar.gz
+Source:		/tmp/cegcc-src-0.12.tar.gz
 # BuildRoot:	/tmp/cegcc
 
 %description
@@ -27,35 +27,45 @@ CeGCC
 #	RPM_ARCH=i386
 #	
 %prep
-%setup -n cegcc-0.11
+%setup -n cegcc-0.12
 
 echo script
 
 %build
 export MY_RPM_PREFIX=/tmp/rpm-$RPM_PACKAGE_NAME-$RPM_PACKAGE_RELEASE
-sh scripts/linux/rpm-build.sh
+cd src || exit 1
+sh build-cegcc.sh all || exit 1
+sh build-mingw32ce.sh all || exit 1
 
 %install
-#
-# No special prefix here
-#
-export PATH=/tmp/rpm-$RPM_PACKAGE_NAME-$RPM_PACKAGE_RELEASE/bin:$PATH
-sh scripts/linux/rpm-install.sh
+# Nothing to do
 
 %files
-/usr/ppc/arm-wince-cegcc
-/usr/ppc/arm-wince-mingw32ce
-%attr(755, root, root) /usr/ppc/bin
-/usr/ppc/include
-/usr/ppc/lib
-/usr/ppc/libexec
-/usr/ppc/share
-/usr/ppc/COPYING
-/usr/ppc/COPYING.LIB
-/usr/ppc/NEWS
-/usr/ppc/README
+/opt/cegcc/arm-wince-cegcc
+/opt/mingw32ce/arm-wince-mingw32ce
+%attr(755, root, root) /opt/cegcc/bin
+/opt/cegcc/include
+/opt/cegcc/lib
+/opt/cegcc/libexec
+/opt/cegcc/share
+/opt/cegcc/COPYING
+/opt/cegcc/COPYING.LIB
+/opt/cegcc/NEWS
+/opt/cegcc/README
+%attr(755, root, root) /opt/mingw32ce/bin
+/opt/mingw32ce/include
+/opt/mingw32ce/lib
+/opt/mingw32ce/libexec
+/opt/mingw32ce/share
+/opt/mingw32ce/COPYING
+/opt/mingw32ce/COPYING.LIB
+/opt/mingw32ce/NEWS
+/opt/mingw32ce/README
 
 %changelog
+* Sat Dec 30 2006 Danny Backx <dannybackx@users.sf.net>
+- Adapt to Pedro's build scripts and /opt/cegcc and /opt/mingw32ce .
+
 * Wed Nov 1 2006 Danny Backx <dannybackx@users.sf.net>
 - Add COPYING.LIB
 - Increase level to produce a 0.11 version next time.
--- cegcc/scripts/linux/rpm-create-source.sh
+++ cegcc/scripts/linux/rpm-create-source.sh
@@ -1,42 +1,51 @@
 #!/bin/sh
-#
-# Read the settings
-#
-if [ -r settings.sh ]; then
-	. settings.sh
+if [ -x ./rpm-create-source.sh ]; then
+	cd ../..
+	TOP_SRCDIR=`pwd`
 else
-	. scripts/linux/settings.sh
+	TOP_SRCDIR=`pwd`
 fi
 #
-# Put the cleanup here instead of in the calling script
+export TOP_SRCDIR
 #
-if [ -d $BUILD_DIR/gcc ]; then
-	rm -rf $BUILD_DIR/gcc
-fi
+# The name of this release
 #
-# cd $BUILD_DIR
-# make all-gcc || exit 1
+CEGCC_RELEASE=0.12
+export CEGCC_RELEASE
 #
-# Create a description of files not to put in the tar.
+# Don't take unnecessary stuff in the source file.
 #
 echo '*/.svn*' >/tmp/exclude-$$
 echo '/*.cvs*' >>/tmp/exclude-$$
+echo '*/CVS' >>/tmp/exclude-$$
+echo '*/CVS/*' >>/tmp/exclude-$$
+echo 'cegcc-'$CEGCC_RELEASE'/src/build-cegcc' >>/tmp/exclude-$$
+echo 'cegcc-'$CEGCC_RELEASE'/src/build-cegcc/*' >>/tmp/exclude-$$
+echo 'cegcc-'$CEGCC_RELEASE'/src/build-mingw32ce' >>/tmp/exclude-$$
+echo 'cegcc-'$CEGCC_RELEASE'/src/build-mingw32ce/*' >>/tmp/exclude-$$
 echo '*~' >>/tmp/exclude-$$
 #
 cd $TOP_SRCDIR
 ln -s . cegcc-$CEGCC_RELEASE
-tar --exclude-from=/tmp/exclude-$$ \
-	-cz -f /usr/src/RPM/SOURCES/cegcc-src-$CEGCC_RELEASE.tar.gz \
-	cegcc-$CEGCC_RELEASE/NEWS cegcc-$CEGCC_RELEASE/README \
-	cegcc-$CEGCC_RELEASE/scripts cegcc-$CEGCC_RELEASE/docs \
-	cegcc-$CEGCC_RELEASE/website cegcc-$CEGCC_RELEASE/test cegcc-$CEGCC_RELEASE/src
+# tar --exclude-from=/tmp/exclude-$$ \
+# 	-cz -f /usr/src/rpm/SOURCES/cegcc-src-$CEGCC_RELEASE.tar.gz \
+# 	cegcc-$CEGCC_RELEASE/NEWS cegcc-$CEGCC_RELEASE/README \
+# 	cegcc-$CEGCC_RELEASE/scripts cegcc-$CEGCC_RELEASE/docs \
+# 	cegcc-$CEGCC_RELEASE/website cegcc-$CEGCC_RELEASE/test cegcc-$CEGCC_RELEASE/src
 rm cegcc-$CEGCC_RELEASE
 #
 # Remove temp file
 #
 rm -f /tmp/exclude-$$
+#
+# Tell packager what to do.
+#
+echo "Now be sure to build with"
+echo " "
+echo "	rm -rf /opt/cegcc/* /opt/mingw32ce/*"
+echo "	rpmbuild -tb /usr/src/rpm/SOURCES/cegcc-src-"$CEGCC_RELEASE".tar.gz"
+echo " "
+#
+# All done
+#
 exit 0
-#
-# This works
-# tar --exclude-from=scripts/linux/x -cz -f /tmp/cegcc-src-0.06.tar.gz NEWS scripts docs website test
-#
