185c3550632897364e62f329e80c7bc585fff881
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2007-12-16 17:19:43 +0000

    Small changes suggested by Jacek.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@1090 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/test/shared/demodll.c
+++ cegcc/test/shared/demodll.c
@@ -16,11 +16,11 @@ static void show_errmsg_dll(const char *func)
 	snprintf(msg, sizeof(msg), "%s: errno = %i", func, errno);
 #else
 	int e = GetLastError();
-	snprintf(msg, sizeof(msg), "%s: last error = %i", func, e);
+	snprintf(msg, sizeof(msg), "%s: last error = %i ", func, e);
 #endif
 	w_msg = (wchar_t *) malloc(sizeof(wchar_t) * strlen(msg));
 	mbstowcs(w_msg, msg, strlen(msg));
-	MessageBoxW(0, w_msg, NULL, 0);
+	MessageBoxW(0, w_msg, NULL, MB_OK | MB_SETFOREGROUND);
 	free(msg);
 	free(w_msg);
 }
@@ -31,9 +31,9 @@ void doit(const TCHAR *text)
 #if 1
 	TCHAR	buf[32];
 	swprintf(buf, L"Message from DLL (%d)", init);
-	MessageBoxW(0, buf, text, 0);
+	MessageBoxW(0, buf, text, MB_OK | MB_SETFOREGROUND);
 #else
-	MessageBoxW(0, L"Message from DLL", text, 0);
+	MessageBoxW(0, L"Message from DLL", text, MB_OK | MB_SETFOREGROUND);
 #endif
 
 	/* Code from Erik */
