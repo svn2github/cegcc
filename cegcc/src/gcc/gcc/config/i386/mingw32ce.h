d9cfc0f86d11f773596bd68c700bedb21b7edc57
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2009-04-11 14:19:15 +0000

    Add new file, make sure __MSVCRT__ doesn't get predefined.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@1235 d7810a3d-100a-0410-8641-c3624a9c11f1

---
+++ cegcc/src/gcc/gcc/config/i386/mingw32ce.h
@@ -0,0 +1,35 @@
+/* Implement cegcc's flavour of a gcc runtime for Windows CE on x86.
+
+   Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004
+   Free Software Foundation, Inc.
+
+   Copyright (c) 2009, Danny Backx.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING.  If not, write to
+the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.  */
+
+/* Same as for cygwin but without the MSVCRT */
+#undef EXTRA_OS_CPP_BUILTINS
+#define EXTRA_OS_CPP_BUILTINS()					\
+  do								\
+    {								\
+      builtin_define ("__MINGW32__");			   	\
+      builtin_define ("_WIN32");				\
+      builtin_define_std ("WIN32");				\
+      builtin_define_std ("WINNT");				\
+    }								\
+  while (0)
