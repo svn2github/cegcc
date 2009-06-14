5ff967d08aa61dfbc3121fd5fc367dbb2c6321b4
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2009-06-14 13:30:50 +0000

    Add a tool to verify presence of a function in a DLL.
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@1339 d7810a3d-100a-0410-8641-c3624a9c11f1

---
+++ cegcc/tools/dll/testapi.c
@@ -0,0 +1,38 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void (WINAPI *fun)(void);
+
+int main(int argc, char *argv[])
+{
+	int	i;
+	HINSTANCE		dll;
+	wchar_t			dllname[64], wapi[64];
+
+
+	if (argc < 3) {
+		printf("Usage : %s dll-name api-name [api-name ..]\n", argv[0]);
+		exit(0);
+	}
+
+	mbstowcs(dllname, argv[1], 64);
+	dll = LoadLibrary(dllname);
+	if (! dll) {
+		DWORD	e = GetLastError();
+		printf("LoadLibrary(%s) : cannot load DLL -> error %d\n", argv[1], e);
+		exit(1);
+	}
+	for (i=2; i<argc; i++) {
+		mbstowcs(wapi, argv[i], 64);
+		*(FARPROC *)&fun = GetProcAddress(dll, wapi);
+		if (fun) {
+			printf("\t%s implements %s\n", argv[1], argv[i]);
+		} else {
+			DWORD	e = GetLastError();
+			printf("%s doesn't know about %s\n", argv[1], argv[i]);
+		}
+	}
+
+	exit(0);
+}
