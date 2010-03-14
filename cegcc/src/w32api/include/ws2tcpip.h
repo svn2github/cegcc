8142c24b23229d34600c037c8ccd11b9930ce102
dannybackx <dannybackx@d7810a3d-100a-0410-8641-c3624a9c11f1>
2010-03-14 08:07:58 +0000

    Make some functions appear for CE as well
    git-svn-id: https://svn.code.sf.net/p/cegcc/code/trunk@1447 d7810a3d-100a-0410-8641-c3624a9c11f1

--- cegcc/src/w32api/include/ws2tcpip.h
+++ cegcc/src/w32api/include/ws2tcpip.h
@@ -307,7 +307,7 @@ struct addrinfo {
 	struct addrinfo  *ai_next;
 };
 
-#if (_WIN32_WINNT >= 0x0501)
+#if (_WIN32_WINNT >= 0x0501) || (_WIN32_WCE > 0x0400)
 void WSAAPI freeaddrinfo (struct addrinfo*);
 int WSAAPI getaddrinfo (const char*,const char*,const struct addrinfo*,
 		        struct addrinfo**);
